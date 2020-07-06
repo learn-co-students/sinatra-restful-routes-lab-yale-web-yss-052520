211785234389c571b83f7ee5040a1c819a30972b
mlkelly <maurak87@gmail.com>
2020-07-05 23:22:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,51 +4,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/recipes' do
+  get '/recipes' do 
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/new' do
+  get '/recipes/new' do 
     erb :new
   end
 
-  get '/recipes/:id' do
+  get '/recipes/:id' do 
     @recipe = find_recipe
-    @name= @recipe.name
-    @ingredients = @recipe.ingredients
-    @cook_time = @recipe.cook_time
-
     erb :show
   end
 
-  get "/recipes/:id/edit" do
-    @recipe = find_recipe
-    @ingredients = @recipe.ingredients
-
-    erb :edit
-  end 
-
-  post '/recipes' do
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    redirect "/recipes/#{@recipe.id}"
+  post '/recipes' do 
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
   end
 
+  get '/recipes/:id/edit' do 
+    @recipe = find_recipe
+    erb :edit
+  end
+  
   patch '/recipes/:id' do
     @recipe = find_recipe
     @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    redirect "/recipes/#{@recipe.id}"
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id' do
     @recipe = find_recipe
-    @recipe.destory
-    redirect "/recipes"
+    @recipe.destroy
+    redirect '/recipes'
   end
 
-  # helper find recipes
+  # helper find method
   def find_recipe
     Recipe.find(params[:id])
   end
-
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200619125640) do
+ActiveRecord::Schema.define(version: 20200616235455) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
